cd8a9de38fecda11504519c2c3fbaa23667fa14f
AntonSyzko <antonio.shizko@gmail.com>
2016-09-27 18:12:17 +0100

    Deploy

--- src/main/java/com/antonsyzko/config/MvcConfig.java
+++ src/main/java/com/antonsyzko/config/MvcConfig.java
@@ -7,11 +7,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.datetime.DateFormatter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    //todel
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**") .addResourceLocations("/resources/");
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
