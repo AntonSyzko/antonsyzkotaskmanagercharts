b4df250732e9a7d27733d87e6d6df0b631562120
AntonSyzko <antonio.shizko@gmail.com>
2016-10-15 20:41:34 +0100

    Update NotificationService.java

--- src/main/java/com/antonsyzko/service/NotificationService.java
+++ src/main/java/com/antonsyzko/service/NotificationService.java
@@ -1,6 +1,5 @@
 package com.antonsyzko.service;
 
-
 import com.antonsyzko.entity.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
@@ -8,9 +7,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-
 import java.util.Date;
-
 /**
  * Created by Admin on 21.09.2016.
  */
@@ -26,14 +23,9 @@ public class NotificationService {
 
     @Async
     public void sendNotificaitoin(Task task) throws MailException, InterruptedException {
-
-
-
         System.out.println("Sleeping now...");
         Thread.sleep(1000);
-
         System.out.println("Sending email...");
-
         SimpleMailMessage mail = new SimpleMailMessage();
         mail.setTo(task.getEmail());
         mail.setFrom("antonio.shizko@gmail.com");
@@ -41,9 +33,8 @@ public class NotificationService {
         System.out.println(task.getCreatedAt());
         mail.setText("\r\nYour current task for today is \r\n" + task.getDescription() + "\r\n priority: " +
                 task.getTaskPriority() + "\r\n current Status: " + task.getTaskStatus()+
-        "\r\n Task has to be done due to "+task.getDueTo());
+                "\r\n Task has to be done due to "+task.getDueTo());
         javaMailSender.send(mail);
-
         System.out.println("Email Sent!");
     }
 }
