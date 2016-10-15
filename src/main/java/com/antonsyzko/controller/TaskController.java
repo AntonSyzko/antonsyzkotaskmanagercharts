2f387cf0e1cd45f93dab135991aa8b2c4ce40f67
AntonSyzko <antonio.shizko@gmail.com>
2016-10-15 20:38:46 +0100

    Update TaskController.java

--- src/main/java/com/antonsyzko/controller/TaskController.java
+++ src/main/java/com/antonsyzko/controller/TaskController.java
@@ -1,12 +1,7 @@
 package com.antonsyzko.controller;
-
 /**
  * Created by Admin on 16.09.2016.
  */
-
-
-
-
 import com.antonsyzko.entity.Task;
 import com.antonsyzko.repository.TaskRepository;
 import com.antonsyzko.service.NotificationService;
@@ -26,7 +21,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import javax.validation.Valid;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -37,29 +31,22 @@ import java.util.List;
 @RequestMapping("/")
 @EnableWebSecurity
 public class TaskController extends WebMvcConfigurerAdapter {
-
-
     Logger logger = LoggerFactory.getLogger(TaskController.class);
     @Autowired
     private TaskRepository repository;
     @Autowired
     private TaskServiceImpl service;
-
     @Autowired
     private NotificationService notificationService;
-
 
     @InitBinder
     public void initBinder(WebDataBinder binder) {
         CustomDateEditor editor = new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd"), true);
         binder.registerCustomEditor(Date.class, editor);
-
     }
 
-    // Login form
     @RequestMapping("/login")
     public String login() {
-
         return "login";
     }
 
@@ -72,8 +59,6 @@ public class TaskController extends WebMvcConfigurerAdapter {
     public String rombsPolars(Model model) {
         return "rombspolars";
     }
-
-
 
     @RequestMapping(method = RequestMethod.GET)
     public String findAll(Model model) {
@@ -91,13 +76,11 @@ public class TaskController extends WebMvcConfigurerAdapter {
     @RequestMapping(method = RequestMethod.DELETE)
     public String delete(@RequestParam Long id,RedirectAttributes redirectAttributes) {
         service.delete(id);
-        redirectAttributes.addFlashAttribute("deleted",
-                " Task with ID number :   " + id + " has  been deleted ") ;
+        redirectAttributes.addFlashAttribute("deleted"," Task with ID number :   " + id + " has  been deleted ") ;
         return "redirect:/";
     }
 
     @RequestMapping(method = RequestMethod.POST)
-
     public String post(@Valid @ModelAttribute("newTask") Task task, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
         if (bindingResult.hasErrors()) {
             model.addAttribute("newTask", task);
@@ -106,28 +89,17 @@ public class TaskController extends WebMvcConfigurerAdapter {
         } else {
             service.create(task);
             try {
-
-
                notificationService.sendNotificaitoin(task);
-
-
-                redirectAttributes.addFlashAttribute("message",
+               redirectAttributes.addFlashAttribute("message",
                         "TASK HAS BEEN SUCCESSFULLY SENT TO      " + task.getEmail()) ;
-
             }catch( Exception e ){
-                // catch error
                logger.info("Error Sending Email: " + e.getMessage());
-                redirectAttributes.addFlashAttribute("failed",
+               redirectAttributes.addFlashAttribute("failed",
                         " delivery to  " + task.getEmail() + " failed" + e.getMessage()) ;
             }
-
-
-
             return "redirect:/";
         }
     }
-
-
 
     @ModelAttribute("taskStatus")
     public List<String> statusList() {
@@ -154,24 +126,27 @@ public class TaskController extends WebMvcConfigurerAdapter {
         list.add("team_lead@gmail.com");
         list.add("project_manager@gmail.com");
         list.add("antonsyzko@gmail.com");
-
         return list;
     }
+    
     @ModelAttribute("memail")
     public int meMailList(){
         int  result= repository.findByEmail("antonio.shizko@gmail.com").size();
         return result;
     }
+    
     @ModelAttribute("testerqa")
     public int testerQAList(){
         int  result= repository.findByEmail("antonsyzko@gmail.com").size();
         return result;
     }
+    
     @ModelAttribute("javadevmail")
     public int javaDevMailList(){
         int  result= repository.findByEmail("java_dev@gmail.com").size();
         return result;
     }
+    
     @ModelAttribute("teamleadmail")
     public int teamLeadMailList(){
         int  result= repository.findByEmail("team_lead@gmail.com").size();
@@ -188,39 +163,42 @@ public class TaskController extends WebMvcConfigurerAdapter {
      int  result= repository.findByTaskPriority("HIGH").size();
         return result;
     }
+    
     @ModelAttribute("lowpriority")
     public int lowPriorityList(){
         int  result= repository.findByTaskPriority("LOW").size();
         return result;
     }
+    
     @ModelAttribute("mediumpriority")
     public int mediumPriorityList(){
         int  result= repository.findByTaskPriority("MEDIUM").size();
         return result;
     }
+    
     @ModelAttribute("startedstatus")
     public int startedStatusList(){
         int  result= repository.findByTaskStatus("STARTED").size();
         return result;
     }
+    
     @ModelAttribute("donestatus")
     public int doneStatusList(){
         int  result= repository.findByTaskStatus("DONE").size();
         return result;
     }
+    
     @ModelAttribute("inprocessstatus")
     public int inProcessStatusList(){
         int  result= repository.findByTaskStatus("IN PROCESS").size();
         return result;
     }
+    
     @ModelAttribute("statall")
     public int statisticsList(){
         int  result= repository.findAll().size();
         return result;
     }
-
-
-
 
     @ExceptionHandler({TaskNotFoundException.class})
     public ModelAndView getSuperheroesUnavailable(TaskNotFoundException ex) {
