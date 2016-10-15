fde655dbc421f99d8eca86184c74d1837322a8a7
AntonSyzko <antonio.shizko@gmail.com>
2016-10-15 20:42:00 +0100

    Update TaskNotFoundException.java

--- src/main/java/com/antonsyzko/service/TaskNotFoundException.java
+++ src/main/java/com/antonsyzko/service/TaskNotFoundException.java
@@ -1,11 +1,8 @@
 package com.antonsyzko.service;
-
 /**
  * Created by Admin on 16.09.2016.
  */
-
 public class TaskNotFoundException extends RuntimeException {
-
     public TaskNotFoundException(String message) {
         super(message);
     }
